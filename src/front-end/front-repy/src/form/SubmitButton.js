675e3fb390f809884086da9c94fc6d7201f4aa13
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-05-12 22:49:31 -0300

    Componente SubmitButton

---
+++ src/front-end/front-repy/src/form/SubmitButton.js
@@ -0,0 +1,13 @@
+
+
+function SubmitButton({text}) {
+    return (
+        <div>
+            <button>
+                {text}
+            </button>
+        </div>
+    );
+}
+
+export default SubmitButton;
