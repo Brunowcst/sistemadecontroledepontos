892e06b7f96d992e16263fe3f184e069577e599a
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-05-13 00:31:05 -0300

    Att Input

--- src/front-end/front-repy/src/form/Input.js
+++ src/front-end/front-repy/src/form/Input.js
@@ -1,8 +1,8 @@
-
+import styles from './styles/Input.module.css';
 
 function Input({type, text, placeholder, name, value, handleOnChange}) {
     return (
-        <div>
+        <div className={styles.container_input}>
             <label htmlFor={name}>{text}</label>
             <input
                 type={type}
