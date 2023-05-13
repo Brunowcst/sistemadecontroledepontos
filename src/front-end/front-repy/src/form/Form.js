5eba3d7c73277f14e4ec4a66bb24e212508ed7ab
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-05-13 01:27:16 -0300

    Visibility button

--- src/front-end/front-repy/src/form/Form.js
+++ src/front-end/front-repy/src/form/Form.js
@@ -2,12 +2,23 @@ import Input from './Input';
 import SubmitButton from './SubmitButton';
 import styles from './styles/Form.module.css';
 import {AiOutlineEye} from 'react-icons/ai';
+import {AiOutlineEyeInvisible} from 'react-icons/ai';
+import { useState } from 'react';
 
 function Form({btnText, handleSubmit}) {
+    const [passwordVisible, setPasswordVisible] = useState(false);
+    const [vibilityIcon, setVisibilityIcon] = useState(<AiOutlineEye />);
+
 
     const submit = (e) => {
         e.preventDefault();
     }
+
+    const togglePasswordVisibility = () => {
+        //console.log(passwordVisible);
+        setPasswordVisible((prevState) => !prevState);
+        setVisibilityIcon(passwordVisible ? <AiOutlineEye /> : <AiOutlineEyeInvisible />);
+    };
 
     return (
         <form className={styles.container_form} onSubmit={submit}>
@@ -23,16 +34,16 @@ function Form({btnText, handleSubmit}) {
 
            <div className={styles.input_container}>
                <Input
-               type="password"
+               type={passwordVisible ? 'text' : 'password'}
                text="Senha"
-               id="password"
+               id="passwordInput"
                name="password"
                placeholder="Digite sua senha"
                //onChange={handleOnChange}
                //value=
                />
 
-            <AiOutlineEye className={styles.icon}/>
+            <span className={styles.icon} onClick={togglePasswordVisibility}>{vibilityIcon}</span>
            </div>
 
            <p className={styles.forget}>Forget password?</p>
