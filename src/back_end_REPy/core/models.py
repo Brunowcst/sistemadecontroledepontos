1db90c134f493aeacf079140a2414415d9531f33
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-05-17 08:04:34 -0300

    Correção models

--- src/back_end_REPy/core/models.py
+++ src/back_end_REPy/core/models.py
@@ -5,10 +5,10 @@ class Funcionario(models.Model):
     nome = models.CharField(max_length = 100, null = False, default='nome')
     cpf = models.CharField(max_length = 11, unique = True, null = False,  default='cpf')
     sexo = models.CharField(max_length = 1)
-    data_nasc = models.DateField(null = False,  default='0000/00/00')
-    cod_gerente = models.ForeignKey( 'self', on_delete = models.CASCADE, default = 0)
-    cod_depto = models.ForeignKey('Departamento', on_delete = models.CASCADE, default = 0)
-    cod_cargo = models.ForeignKey('Cargo', on_delete = models.CASCADE, default = 0)
+    data_nasc = models.DateField(null = False,  default= 'YYYY-DD-MM')
+    cod_gerente = models.ForeignKey(to='self', on_delete = models.CASCADE, null = True, blank= True)
+    cod_depto = models.ForeignKey(to='Departamento', on_delete = models.CASCADE, null = True, blank= True)
+    cod_cargo = models.ForeignKey(to='Cargo', on_delete = models.CASCADE)
 
     def __str__(self):
         return self.nome
@@ -18,7 +18,7 @@ class Usuario(models.Model):
     email = models.CharField(max_length = 30, null = False, unique = True,  default='email')
     senha = models.CharField( max_length = 12, null = False,  default='password' )
     token = models.CharField( max_length = 12, null = False, unique = True,  default='0000' )
-    cod_func = models.ForeignKey( Funcionario, on_delete = models.CASCADE, default = 0)
+    cod_func = models.ForeignKey(to= Funcionario, on_delete = models.CASCADE, null= True, blank= True)
 
     def __str__(self):
         return self.usuario
@@ -26,8 +26,8 @@ class Usuario(models.Model):
 class Departamento(models.Model):
     nome = models.CharField(max_length = 50, null = False, default='Depto_nome')
     sigla = models.CharField(max_length = 10, null = False, default='')
-    data_criacao = models.DateField( null = False,  default='0000/00/00')
-    cod_gerente = models.ForeignKey( Funcionario, on_delete = models.CASCADE)
+    data_criacao = models.DateField( null = False,  default='2000-01-01')
+    cod_gerente = models.ForeignKey( to= Funcionario, on_delete = models.CASCADE)
 
     def __str__(self):
         return self.nome
@@ -42,7 +42,7 @@ class Cargo(models.Model):
 
 class Ponto(models.Model):
     descricao = models.CharField(max_length = 255,  default='')
-    data = models.DateField( null = False,  default='0000/00/00')
+    data = models.DateField( null = False,  default='2000-01-01')
     cod_func = models.ForeignKey( Funcionario, on_delete = models.CASCADE)
     cor_turno = models.ForeignKey('Turno', on_delete = models.CASCADE)
 
@@ -55,9 +55,11 @@ class Turno(models.Model):
     hora_fim = models.CharField(max_length = 12, null = False,  default='00')
 
     def __str__(self):
-        return "%s, %s-%s" %(self.nome,self.hora_inicio, self.hora_fim)
-
+        return "%s | %s - %s" %(self.sigla, self.hora_inicio, self.hora_fim)
+        
 class Horario(models.Model):
     dia = models.CharField(max_length = 10, null = False,  default='24h')
     hora_entrada = models.CharField(max_length = 12, null = False,  default='0h')
     hora_saida = models.CharField(max_length = 12, null = False,  default='24h')
+    def __str__(self):
+        return self.dia
