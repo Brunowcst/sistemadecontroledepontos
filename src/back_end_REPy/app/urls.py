9b2454c3a64abe0304b74c8a202a168ff8f4e99a
david <david.emanoel.706@ufrn.edu.br>
2023-05-18 13:42:33 -0300

    Update

--- src/back_end_REPy/app/urls.py
+++ src/back_end_REPy/app/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from rest_framework import routers, serializers, viewsets
 from core.views import FuncionarioViewSet, DepartamentoViewSet, UsuarioViewSet, CargoViewSet, PontoViewSet, TurnoViewSet, HorarioViewSet
@@ -31,6 +32,6 @@ router.register(r'horario', HorarioViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
     path('admin/', admin.site.urls)
 ]
