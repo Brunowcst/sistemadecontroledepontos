3598bd618c71e3cbc02642f4cd9a3d3ffc03214c
david <david.emanoel.706@ufrn.edu.br>
2023-05-28 18:36:48 -0300

    fix-test

---
+++ src/back_end_REPy/app_api/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Funcionario, Departamento, Usuario, Ponto, Horario, Cargo, Turno
+
+# Register your models here.
+# admin.site.register(Funcionario) 
+# admin.site.register(Departamento)
+# admin.site.register(Usuario)
+# admin.site.register(Ponto)
+# admin.site.register(Horario)
+# admin.site.register(Cargo)
+# admin.site.register(Turno)
