ed2d29b496764b2093884ef9ccd2a998053b2330
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-06-03 14:27:45 -0300

    Nova rota front

--- src/front-end/front-repy/src/routes/Routes.js
+++ src/front-end/front-repy/src/routes/Routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom
 import Login from '../pages/Login';
 import Cadastro from '../pages/Cadastro';
 import MainLayout from '../layout/MainLayout';
+import RecuperarAcesso from '../pages/RecuperarAcesso';
 
 function routes(props) {
     return (
@@ -9,6 +10,7 @@ function routes(props) {
             <Outlet/>
             <Routes>
                 <Route path='/' element={<Login/>} />
+                <Route path='/recuperar-acesso' element={<RecuperarAcesso/>}/>
                 <Route element={<MainLayout/>}>
                     <Route path="/cadastro" element={<Cadastro />} />
                 </Route>
