a4a2aa0e336fba6cac07a99533bf0d9661b00fc9
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-06-10 13:09:47 -0300

    Adiciona configurações

--- src/back_end_REPy/app/settings.py
+++ src/back_end_REPy/app/settings.py
@@ -50,8 +50,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'rest_framework',
+    "rest_framework_simplejwt.token_blacklist",
+
     'core',
+
     'corsheaders',
 ]
 
@@ -60,7 +64,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
     'corsheaders.middleware.CorsMiddleware',
-
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -69,24 +73,18 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# CORS_ORIGIN_WHITELIST = [
-#     'http://localhost:3000',
-# ]
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]
 
-CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']  # Adicione o URL do seu aplicativo React aqui
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:3000',
+    ]  # Adicione o URL do seu aplicativo React aqui
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',  # Adicione o URL do seu aplicativo React aqui
+    'http://localhost:3000',
+      # Adicione o URL do seu aplicativo React aqui
 ]
-
-CORS_ALLOW_METHODS = [
-    'GET',
-    'POST',
-    'OPTIONS',
-]
-
-CSRF_COOKIE_NAME = 'csrftoken'  # Nome do cookie CSRF
-CSRF_HEADER_NAME = 'X-Csrftoken'  # Nome do cabeçalho CSRF
 
 ROOT_URLCONF = 'app.urls'
 
@@ -112,7 +110,6 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# config = config('.env')
 NAME = config('NAME')
 USER = config('USER')
 PASSWORD = config('PASSWORD')
@@ -152,11 +149,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'core.Usuario'
 
-# AUTHENTICATION_BACKENDS = [
-#     'core.back.autent.CustomAuthBackend',  # Substitua 'myapp' pelo nome do seu aplicativo
-#     'django.contrib.auth.backends.ModelBackend',
-# ]
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -180,8 +172,42 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "UPDATE_LAST_LOGIN": False,
+
+    "ALGORITHM": "HS256",
+    "VERIFYING_KEY": "",
+    "AUDIENCE": None,
+    "ISSUER": None,
+    "JSON_ENCODER": None,
+    "JWK_URL": None,
+    "LEEWAY": 0,
+
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+    "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
+
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    "TOKEN_TYPE_CLAIM": "token_type",
+    "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
+
+    "JTI_CLAIM": "jti",
+
+    "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
+    "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
+
+    "TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainPairSerializer",
+    "TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSerializer",
+    "TOKEN_VERIFY_SERIALIZER": "rest_framework_simplejwt.serializers.TokenVerifySerializer",
+    "TOKEN_BLACKLIST_SERIALIZER": "rest_framework_simplejwt.serializers.TokenBlacklistSerializer",
+    "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
+    "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
