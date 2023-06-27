c02930574e94339c42b145153f61995e705a053f
david <david.emanoel.706@ufrn.edu.br>
2023-06-27 11:55:32 -0300

    urls updated

--- src/back_end_REPy/app/urls.py
+++ src/back_end_REPy/app/urls.py
@@ -1,21 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers, serializers, viewsets
-from core.views import FuncionarioViewSet, FuncionarioList, FuncionarioDetail,DepartamentoViewSet, DepartamentoList, DeptoDetail, UsuarioViewSet, CargoViewSet, CargoDetail, CargoList, PontoViewSet, TurnoViewSet, HorarioViewSet, login_view
+from core.views import FuncionarioList, FuncionarioDetail, DeptoList, DeptoDetail, CargoDetail, CargoList, PontoDetail, PontoDetail, PontoList, login_view
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import views as auth_views
 
-router = routers.DefaultRouter()
-router.register(r'funcionario', FuncionarioViewSet)
-router.register(r'departamento', DepartamentoViewSet)
-router.register(r'usuario', UsuarioViewSet)
-router.register(r'cargo', CargoViewSet)
-router.register(r'ponto', PontoViewSet)
-router.register(r'turno', TurnoViewSet)
-router.register(r'horario', HorarioViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'funcionario', FuncionarioViewSet)
+# router.register(r'departamento', DepartamentoViewSet)
+# router.register(r'usuario', UsuarioViewSet)
+# router.register(r'cargo', CargoViewSet)
+# router.register(r'ponto', PontoViewSet)
+# router.register(r'turno', TurnoViewSet)
+# router.register(r'horario', HorarioViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api/login/', csrf_exempt(login_view), name='login'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
@@ -24,18 +24,22 @@ urlpatterns = [
     path('reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     
     ## Funcionarios
-    path('listar_funcionarios/', FuncionarioList.as_view()),
-    path('get_funcionario/<str:cpf>/', FuncionarioDetail.as_view()),
+    path('funcionario/', FuncionarioList.as_view()),
+    path('funcionario/<str:cpf>/', FuncionarioDetail.as_view()),
     
     ## Deptos
-    path('listar_deptos/', DepartamentoList.as_view()),
-    path('get_depto/<str:nome>/',DeptoDetail.as_view()),
+    path('departamento/', DeptoList.as_view()),
+    path('departamento/<str:nome>/',DeptoDetail.as_view()),
 
     ## Cargos
-    path('listar_cargos/', CargoList.as_view()),
-    path('get_cargo/<int:id>/',CargoDetail.as_view()),
+    path('cargo/', CargoList.as_view()),
+    path('cargo/<int:id>/', CargoDetail.as_view()),
+    # path('delete_cargo/<int:id>/', CargoDetail.delete()),
 
     ## Horario
     
+    ## Ponto
+    path('ponto/', PontoList.as_view()),
+    path('ponto/<int:id>/', PontoDetail.as_view()),
 
 ]
