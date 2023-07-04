3c757036098d507cde8b5adc4dca0d5c4aab946f
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-07-04 16:39:59 -0300

    Página de histórico

---
+++ src/front-end/front-repy/src/pages/Historico.js
@@ -0,0 +1,13 @@
+import styles from './styles/Historico.module.css'
+
+function Historico() {
+    return (
+        <div className={styles.grid}>
+            <section className={styles.section}>
+                <h1>Histórico marcações</h1>
+            </section>
+        </div>
+    );
+}
+
+export default Historico;
