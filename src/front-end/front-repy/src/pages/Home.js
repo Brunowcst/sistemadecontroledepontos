ae11e61d43c8a0a96eb6f27bd4b7dba2b25bbc5b
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-07-06 19:10:43 -0300

    Tabela de informações

--- src/front-end/front-repy/src/pages/Home.js
+++ src/front-end/front-repy/src/pages/Home.js
@@ -7,7 +7,45 @@ function Home() {
     
     return (
         <div className={styles.grid}>
-            olá, {name}
+            <h1>Inicio</h1>
+            <section className={styles.sectionUser}>
+                <div className={styles.sectionContainers}>
+                    <p>Nome: -</p>
+                    <p>Cargo: -</p>
+                </div>
+
+                <div className={styles.sectionContainers}>
+                    <p>Cadastro: -</p>
+                </div>
+
+                <div className={styles.sectionContainers}>
+                    <p>Escala: -</p>
+                    <p>Turno: -</p>
+                </div>
+            </section>
+
+            <section className={styles.sectionInformations}>
+                <h2>Dias apurados</h2>
+                <div className={styles.navInformations}>
+                    <p>Todos(-)</p>
+                    <p>Pendentes</p>
+                    <p>Banco de horas</p>
+                </div>
+                
+                <table>
+                    <tr>
+                        <th>Data</th>
+                        <th>Marcações</th>
+                        <th>Situação</th>
+                    </tr>
+                    <tr>
+                        <td>--/--/---</td>
+                        <td>---------</td>
+                        <td>Pendente</td>
+                    </tr>
+                </table>
+
+            </section>
         </div>
     );
 }
