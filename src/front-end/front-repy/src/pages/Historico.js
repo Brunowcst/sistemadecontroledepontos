6bf3898ef104f7348c6a05b78cb793d0bf17f1ef
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-07-07 16:16:21 -0300

    Correções na página de histórico

--- src/front-end/front-repy/src/pages/Historico.js
+++ src/front-end/front-repy/src/pages/Historico.js
@@ -1,25 +1,7 @@
-import { useContext, useEffect, useState } from 'react';
-import AuthContext from '../context/AuthContext';
 import styles from './styles/Historico.module.css'
-import { getFuncionarios } from '../api/funcionarios';
 
 function Historico() {
-    const [funcionario, setFuncionarios] = useState([])
-    const {bearer} = useContext(AuthContext)
-
-    useEffect(() => {
-        const dataFuncionarios = async () => {
-            try {
-                const data = await getFuncionarios({bearer});
-                setFuncionarios(data)
-            } catch (error) {
-                console.log(error)
-            }
-        };
-
-        dataFuncionarios();
-    }, []);
-
+    
     
     return (
         <div className={styles.grid}>
@@ -27,13 +9,6 @@ function Historico() {
                 <h1>Histórico marcações</h1>
             </section>
 
-            {funcionario.length > 0 && (
-                <div>
-                    {funcionario.map((func) => {
-                       return <div>{func.nome}</div>
-                    })}
-                </div>
-            )}
         </div>
     );
 }
