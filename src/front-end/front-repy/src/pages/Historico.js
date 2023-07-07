5b4f0fa4a50f88ebd7463b3966dd3ce156448369
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-07-07 00:13:28 -0300

    Requisição de funcionários

--- src/front-end/front-repy/src/pages/Historico.js
+++ src/front-end/front-repy/src/pages/Historico.js
@@ -1,11 +1,50 @@
+import { useContext, useEffect, useState } from 'react';
+import AuthContext from '../context/AuthContext';
 import styles from './styles/Historico.module.css'
 
 function Historico() {
+    const [funcionario, setFuncionarios] = useState([])
+    const {bearer} = useContext(AuthContext)
+
+    useEffect(() => {
+        const getFuncionarios = async () => {
+    
+            try {
+                let response = await fetch("http://localhost:8000/funcionario/", {
+                  method: 'GET',
+                  headers: {
+                    'Content-type' : 'application/json',
+                    'Authorization' : 'Bearer ' + bearer
+                  },
+                }).then((resp) => resp.json()
+                    .then((data) => {
+                        setFuncionarios(data)
+                        console.log('func '+ funcionario)
+                    })
+                );
+
+            } catch (error) {
+                console.log(error);
+            }
+        }    
+
+        getFuncionarios()
+    }, []);
+
+    
     return (
         <div className={styles.grid}>
             <section className={styles.section}>
                 <h1>Histórico marcações</h1>
             </section>
+
+            {funcionario.length > 0 && (
+                <div>
+                    {funcionario.map((func) => {
+                       return <div>{func.nome}</div>
+                    })}
+                </div>
+            )}
         </div>
     );
 }
