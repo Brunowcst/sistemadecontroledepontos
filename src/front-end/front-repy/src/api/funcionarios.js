4fa717068ce3b694170cb386da3270a770bf1610
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-07-07 15:44:02 -0300

    Requisições para api

---
+++ src/front-end/front-repy/src/api/funcionarios.js
@@ -0,0 +1,24 @@
+import {base} from '../api/base'
+
+export const getFuncionarios = async ({bearer}) => {
+    
+    try {
+        let response = await fetch(`${base}/funcionario/`, {
+          method: 'GET',
+          headers: {
+            'Content-type' : 'application/json',
+            'Authorization' : 'Bearer ' + bearer
+          },
+        });
+        let data = await response.json();
+        if(response.status === 200) {
+            return data
+        } else {
+            throw new Error('Error: requisição de dados.')
+        }
+    } catch (error) {
+        console.log(error)
+        throw error;
+    }
+}    
+
