909a88835fb6ad03489af2b32de898f0bf5990ac
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-07-07 16:28:44 -0300

    Página de cadastros

---
+++ src/front-end/front-repy/src/pages/Cadastro.js
@@ -0,0 +1,15 @@
+import styles from './Cadastro.module.css'
+import FormFuncionario from '../form/FormFuncionario'
+
+function Cadastro() {
+    return (
+        <div className={styles.grid}>
+            <section className={styles.section}>
+                <h1>Cadastrar funcionário</h1>
+                <FormFuncionario btnText="Cadastrar"/>
+            </section>
+        </div>
+    );
+}
+
+export default Cadastro;
