018fb473d9520e1cab3f5835c33838c79106f44c
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-07-10 20:12:45 -0300

    Página de detalhes do funcionário

---
+++ src/front-end/front-repy/src/pages/FuncionarioDetalhes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './styles/FuncionarioDetalhes.module.css'
+
+function FuncionarioDetalhes(props) {
+    return (
+        <div className={styles.grid}>
+            <section className={styles.section}>
+            </section>
+        </div>
+    );
+}
+
+export default FuncionarioDetalhes;
