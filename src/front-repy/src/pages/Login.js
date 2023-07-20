0fcc406997b6aaa76d1c189fca0972c7bf5d736d
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-07-19 22:27:47 -0300

    correção login

--- src/front-repy/src/pages/Login.js
+++ src/front-repy/src/pages/Login.js
@@ -2,6 +2,7 @@ import styles from './styles/Login.module.css';
 import Form from '../form/Form';
 import {BsFacebook} from 'react-icons/bs';
 import {FcGoogle} from 'react-icons/fc';
+import Logo from '../material/images/logorepy.svg'
 
 
 function Login() {
@@ -10,15 +11,16 @@ function Login() {
         <>
             <div className={styles.container}>
                 <div className={styles.container_login}>
+                    <img src={Logo} alt='Logo' height={60} className={styles.logo}/>
                     <h1>Login Now</h1> 
                     <Form btnText="Logar"/>
-                    <div>
+                    {/* <div>
                         <p className={styles.textSign}>Ou entre com:</p>
                         <div className={styles.icons_container}>
                             <BsFacebook className={styles.icon}/>
                             <FcGoogle className={styles.icon_google}/>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </>
